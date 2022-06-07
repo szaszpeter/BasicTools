65aa4624f0dd9e014118f51c16495881133bd273
Peter Szasz <szasz.peter@ymail.com>
2022-06-07 13:36:36 +0200

    Add Google Espresso Dependency & Sample Test Method

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.myapplication.navigation.LauncherActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,9 +22,17 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.myapplication", appContext.packageName)
+    }
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LauncherActivity::class.java)
+    @Test fun sampleTestMethod() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.myapplication", appContext.packageName)
     }
